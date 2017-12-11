11f2ec23622613a45320a99ca662e52bf58dfa8d
Marcin Hoinka <mhoinka@gmail.com>
2017-12-11 12:56:44 +0100

    Dodana funkcjonalnośc przcisku usuń

--- src/app/controller/ProjektController.java
+++ src/app/controller/ProjektController.java
@@ -1,27 +1,37 @@
 package app.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import app.database.DBConnection;
+import app.model.Projekt;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 
 public class ProjektController {
 
     @FXML
-    private TableView<?> t_projekty;
+    private TableView<Projekt> t_projekty;
 
     @FXML
-    private TableColumn<?, ?> t_id_p;
+    private TableColumn<Projekt,Integer> t_id_p;
 
     @FXML
-    private TableColumn<?, ?> t_temat;
+    private TableColumn<Projekt, String> t_temat;
 
     @FXML
-    private TableColumn<?, ?> t_opis;
+    private TableColumn<Projekt, String> t_opis;
 
     @FXML
-    private TableColumn<?, ?> t_deadline;
+    private TableColumn<Projekt, String> t_deadline;
 
     @FXML
     private Button btn_dodaj;
@@ -32,13 +42,49 @@ public class ProjektController {
     @FXML
     private Button btn_delete;
 
+
+    
+  public ObservableList<Projekt> dane = FXCollections.observableArrayList();
+  PreparedStatement ps;
+  DBConnection db;
+  Connection conn;
+  int id_selected;
+    
+    
+    
     @FXML
     void addAction(MouseEvent event) {
-
     }
 
+    
+    
+    
+    
+    
     @FXML
     void deleteAction(MouseEvent event) {
+    	
+    	try {
+    	    	id_selected = t_projekty.getSelectionModel().getSelectedItem().getId(); 
+    	    	}
+    	    	catch (Exception e) {
+    	    		Alert a = new Alert(AlertType.ERROR);
+    	        	a.setTitle("B³¹d");
+    	        	a.setHeaderText("B³¹d");
+    	        	a.setContentText("Aby usun¹æ rekord musisz go najpierw zaznaczyæ");
+    	        	a.showAndWait();
+    	    	}
+    	    	
+    	    	connection();
+    	    	try {
+    	    	ps = conn.prepareStatement("DELETE FROM ankieta WHERE id=?");
+    	    	ps.setInt(1, id_selected);
+    	    	ps.executeUpdate();
+    	    	}
+    	    	catch (SQLException e) {
+    	    		e.printStackTrace();
+    	    	}
+    	    	select();
 
     }
 
---
+++ src/app/database/DBConnector.java
@@ -0,0 +1,43 @@
+package app.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+//	koniecznie klasa publiczna bo inaczej siê nie po³¹czymy 
+	
+	Connection conn;
+	
+	//tworzymy metodê 
+	public DBConnection() {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+//			setAutoCommit(true);
+		} catch(ClassNotFoundException e) {
+			System.out.println("Nie zarejesttrowano klasy sterownika");
+		}
+	}
+	
+//  dla ³adniejszego zapisu zmienne do danych logoawnia deklarujemy w String - CASE SENSITIVE!!!!!! piszemy z ma³ych
+//	metoda zwracaj¹ca biekt klasy connection
+	public Connection connInit() {
+		String url = "jdbc:mysql://localhost:3306/usersjdbc";
+		String user = "root";
+		String pass = "W4rszt4T2017";
+		try {
+		conn = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+		System.out.println("Nie mogê siê po³¹czyæ");
+	}
+		return conn;
+	}
+	
+//	public static void main(String[] args) {
+//		DBConnection db1= new DBConnection();
+//		db1.connInit();
+//		
+//	}
+	
+	
+}
