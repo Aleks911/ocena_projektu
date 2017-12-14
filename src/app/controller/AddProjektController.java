b9e1707fba6231452d587b28d1de595fa14b63a3
Aneta <aneta.zolech@gmail.com>
2017-12-14 21:20:47 +0100

    zmiany w bazie i kontrolerze dodawania projektu

--- src/app/controller/AddProjektController.java
+++ src/app/controller/AddProjektController.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 import app.database.DBConnector;
 import app.model.Projekt;
 import app.database.DBConnector;
@@ -49,7 +53,8 @@ public class AddProjektController {
     	tf_deadline.clear();
     	tf_grupa.clear();
     }
-
+    static int z;
+    ArrayList<Integer> lista = new ArrayList <Integer> ();
     @FXML
     void actionInsert(MouseEvent event) {
     	
@@ -71,8 +76,24 @@ public class AddProjektController {
 	    	ps.setString(3, tf_deadline.getText());
 	    	ps.setString(4, tf_grupa.getText());
 	    	ps.executeUpdate();
+	    	ps = conn.prepareStatement("select id_p from projekt where temat=? and opis=? and deadline=? and id_gr=?");
+	    	ps.setString(1, tf_temat.getText());
+	    	ps.setString(2, ta_opis.getText());
+	    	ps.setString(3, tf_deadline.getText());
+	    	ps.setString(4, tf_grupa.getText());
+	    	ResultSet rs = ps.executeQuery();
+	    	while (rs.next()) {
+	    		z = rs.getInt("id_p");
+	    		System.out.println(z);
+	    	}
+	    	ps = conn.prepareStatement("select id_k from kursant where id_g = (select id_gr from projekt where id_p = ?);");
+	    	ps.setInt(1, z);
+	    	ResultSet rs2 = ps.executeQuery();
+	    	while (rs2.next()) {
+	    		lista.add(rs.getInt("id_k"));
+	    	}
+	    	System.out.println(lista);
 	    	
-	    	System.out.println();
 	    	
 //	    	czyszczenie okna pod wys³aniu ankiety
 	    	ActionEvent ae = new ActionEvent();
@@ -96,42 +117,7 @@ public class AddProjektController {
     	select();
     }
     
-    
-    
-    
-    
-//     private void select() {
-//    	connection();
-//    	String dane;
-//    	dane.clear();
-//    	t_projekty.setItems(dane);
-////    	try {
-//    	ps = conn.prepareStatement("SELECT * FROM projekt;");
-//        ResultSet rs = ps.executeQuery();
-//    	while(rs.next()) {
-//    			dane.add(new Projekt(
-//    					rs.getInt("id_p"),
-//    					rs.getString("temat"),
-//    					rs.getString("opis"),
-//    					rs.getString("deadline"),
-//    					rs.getInt("id_gr")));
-//    	}
-//    	
-////    	wpisujemy vartoœci do obiektów kolumn tabeli
-//    	t_id_p.setCellValueFactory(new PropertyValueFactory<Projekt,Integer>("id_p"));
-//    	t_temat.setCellValueFactory(new PropertyValueFactory<Projekt,String>("temat"));
-//    	t_opis.setCellValueFactory(new PropertyValueFactory<Projekt,String>("opis"));
-//    	t_deadline.setCellValueFactory(new PropertyValueFactory<Projekt,String>("deadline"));
-//    	t_id_gr.setCellValueFactory(new PropertyValueFactory<Projekt,Integer>("id_gr"));
-//    	
-////    	dodanie danych do tabeli view w postaci obiektu ObservableList
-//    	t_projekty.setItems(null);
-//    	t_projekty.setItems(dane);
-//    	}
-//    	catch (SQLException e) {
-//    		e.printStackTrace();
-//    	}
-    	
+  	
     	
     	
     Connection conn;
