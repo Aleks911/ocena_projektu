421e5cc23b8f6dc31f4958037efc136bbfa912d8
Aneta <aneta.zolech@gmail.com>
2017-12-15 08:55:06 +0100

    Poprawianie kontrolera dodawanie projektu

--- src/app/controller/AddProjektController.java
+++ src/app/controller/AddProjektController.java
@@ -5,11 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-
-import app.database.DBConnector;
-import app.model.Projekt;
 import app.database.DBConnector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,140 +12,126 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 
+import javafx.scene.input.MouseEvent;
 
 public class AddProjektController {
 
-    @FXML
-    private TextField tf_temat;
+	@FXML
+	private TextField tf_temat;
 
-    @FXML
-    private TextArea ta_opis;
+	@FXML
+	private TextArea ta_opis;
 
-    @FXML
-    private TextField tf_deadline;
+	@FXML
+	private TextField tf_deadline;
 
-    @FXML
-    private TextField tf_grupa;
+	@FXML
+	private TextField tf_grupa;
 
-    @FXML
-    private Button btn_add;
+	@FXML
+	private Button btn_add;
 
-    @FXML
-    private Button btn_clear;
+	@FXML
+	private Button btn_clear;
 
-    @FXML
-    private Button btn_goback;
+	@FXML
+	private Button btn_goback;
 
-    
-    @FXML
+	@FXML
 
-    void actionClear(MouseEvent event) {
-    	ta_opis.clear();
-    	tf_temat.clear();
-    	tf_deadline.clear();
-    	tf_grupa.clear();
-    }
-    static int z;
-    ArrayList<Integer> lista = new ArrayList <Integer> ();
-    @FXML
-    void actionInsert(MouseEvent event) {
-    	
-    	tf_temat.getText();
-    	ta_opis.getText();
-    	tf_deadline.getText();
-    	tf_grupa.getText();
+	void actionClear(MouseEvent event) {
+		ta_opis.clear();
+		tf_temat.clear();
+		tf_deadline.clear();
+		tf_grupa.clear();
+	}
 
-    	
-    	DBConnector db = new DBConnector();
-    	Connection conn = db.connInit();
-    	PreparedStatement ps;
+	static int z;
+	ArrayList<Integer> lista = new ArrayList<Integer>();
+
+	@FXML
+	void actionInsert(MouseEvent event) {
+
+		tf_temat.getText();
+		ta_opis.getText();
+		tf_deadline.getText();
+		tf_grupa.getText();
+
+		DBConnector db = new DBConnector();
+		Connection conn = db.connInit();
+		PreparedStatement ps;
 		try {
-			ps = conn.prepareStatement(
-					"insert into projekt (temat, opis, deadline, id_gr)" 
-					+ "values (?,?,?,?)");
+			ps = conn.prepareStatement("insert into projekt (temat, opis, deadline, id_gr)" + "values (?,?,?,?)");
 			ps.setString(1, tf_temat.getText());
-	    	ps.setString(2, ta_opis.getText());
-	    	ps.setString(3, tf_deadline.getText());
-	    	ps.setString(4, tf_grupa.getText());
-	    	ps.executeUpdate();
-	    	ps = conn.prepareStatement("select id_p from projekt where temat=? and opis=? and deadline=? and id_gr=?");
-	    	ps.setString(1, tf_temat.getText());
-	    	ps.setString(2, ta_opis.getText());
-	    	ps.setString(3, tf_deadline.getText());
-	    	ps.setString(4, tf_grupa.getText());
-	    	ResultSet rs = ps.executeQuery();
-	    	while (rs.next()) {
-	    		z = rs.getInt("id_p");
-	    		System.out.println(z);
-	    	}
-	    	ps = conn.prepareStatement("select id_k from kursant where id_g = (select id_gr from projekt where id_p = ?);");
-	    	ps.setInt(1, z);
-	    	ResultSet rs2 = ps.executeQuery();
-	    	while (rs2.next()) {
-	    		lista.add(rs.getInt("id_k"));
-	    	}
-	    	System.out.println(lista);
-	    	
-	    	
-//	    	czyszczenie okna pod wys³aniu ankiety
-	    	ActionEvent ae = new ActionEvent();
-	    	clearAction(ae);
-	    	
-		} catch (SQLException e) {
-			e.printStackTrace();
+			ps.setString(2, ta_opis.getText());
+			ps.setString(3, tf_deadline.getText());
+			ps.setString(4, tf_grupa.getText());
+			ps.executeUpdate();
+			ps = conn.prepareStatement("select id_p from projekt where temat=? and opis=? and deadline=? and id_gr=?");
+			ps.setString(1, tf_temat.getText());
+			ps.setString(2, ta_opis.getText());
+			ps.setString(3, tf_deadline.getText());
+			ps.setString(4, tf_grupa.getText());
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				z = rs.getInt("id_p");
+			
 			}
 
-    }
-    
-    
-    @FXML
-    void actionGoBack(MouseEvent event) {
-    	((Node)(event.getSource())).getScene().getWindow().hide();
-    }
+			ps = conn.prepareStatement(
+					"select id_k from kursant where id_g = (select id_gr from projekt where id_p = ?);");
+			ps.setInt(1, z);
+			ResultSet rs2 = ps.executeQuery();
+			while (rs2.next()) {
+				lista.add(rs2.getInt("id_k"));
+			}
+			for (int i : lista) {
+				ps = conn.prepareStatement("insert into ocena values (null, ?, ?, null, null);");
+				ps.setInt(1, z);
+				ps.setInt(2, i);
+				ps.executeUpdate();
+			}
 
+			// czyszczenie okna pod wys³aniu ankiety
+			ActionEvent ae = new ActionEvent();
+			clearAction(ae);
 
-    @FXML
-    void actionRefresh(MouseEvent event) {
-    	select();
-    }
-    
-  	
-    	
-    	
-    Connection conn;
-    
-    private void connection() {
-    	
-       		DBConnector db = new DBConnector();
-     		db = new DBConnector();
-     		conn = db.connInit();
-     	}
-    	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
+	}
+
+	@FXML
+	void actionGoBack(MouseEvent event) {
+		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
+
+	@FXML
+	void actionRefresh(MouseEvent event) {
+		select();
+	}
+
+	Connection conn;
+
+	private void connection() {
+
+		DBConnector db = new DBConnector();
+		db = new DBConnector();
+		conn = db.connInit();
+	}
 
 	private void clearAction(ActionEvent ae) {
 
-		
 	}
 
-	
-	   private void select() {
-	    	connection();
-	    	    	}
-	    
-	    public void initialize() {
-	    	select();
-	    }
-	    	
-	
-	
-	
-	
-	
-	
-	
-	
+	private void select() {
+		connection();
+	}
+
+	public void initialize() {
+		select();
+	}
+
 }
